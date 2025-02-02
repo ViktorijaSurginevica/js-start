38baf1e5f11ff2ab82cc6d4f9439667a0749d1b4
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-02 16:12:24 +0300

    batleship

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-let sum = +prompt("Укажите первоначльную сумму", 1000);
-const percent = 15;
-const result = 2000;
-let count = 0;
-const depositTerm = +prompt("Укажите срок депозита", 10);
-while (count < depositTerm) {
-  sum = sum + sum * (percent / 100);
-  count = count + 1;
-  document.write(count + ": " + sum + "<br>");
-}
+// let sum = +prompt("Укажите первоначльную сумму", 1000);
+// const percent = 15;
+// const result = 2000;
+// let count = 0;
+// const depositTerm = +prompt("Укажите срок депозита", 10);
+// while (count < depositTerm) {
+//   sum = sum + sum * (percent / 100);
+//   count = count + 1;
+//   document.write(count + ": " + sum + "<br>");
+// }
+
+const location1 = 3;
+const location2 = 4;
+const location3 = 5;
+
+let isSunk = false;
+
+while (isSunk === false) {}
