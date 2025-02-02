f78e8ca3364779ebc1c5acadc30cf2b4675c153d
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-02 16:57:31 +0300

    part-new

--- index.js
+++ index.js
@@ -24,17 +24,21 @@ let hits = 0;
 while (isSunk === false) {
   // не забываем преврать строку в число
   guess = +prompt("Fire! Enter a number 0-6");
-  // логическое "или"
-  if (guess === location1 || guess === location2 || guess === location3) {
-    alert("HIT!");
-    // считаем попадания
-    hits = hits + 1;
-    // если подбили
-    if (hits === 3) {
-      isSunk = true;
-      alert("You sank my battleship!");
-    }
+  if (guess < 0 || guess > 6) {
+    alert("Please, enter a valid cell number!");
   } else {
-    alert("MISS!");
+    // логическое "или"
+    if (guess === location1 || guess === location2 || guess === location3) {
+      alert("HIT!");
+      // считаем попадания
+      hits = hits + 1;
+      // если подбили
+      if (hits === 3) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
+    } else {
+      alert("MISS!");
+    }
   }
 }
