1fcfc65a1f4ba1985d1f8536a1957c2edca75a8d
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-03 08:40:15 +0300

    guesses

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
 // }
 
 //позиция коробля
-const randomLoc = 2; // от 0 и до 4
+const randomLoc = Math.floor(Math.random() * 5); // от 0 и до 4
 const location1 = randomLoc;
 const location2 = location1 + 1;
 const location3 = location2 + 1;
@@ -21,6 +21,8 @@ let isSunk = false;
 let guess;
 // добавляем счетчик попаданий
 let hits = 0;
+// создаем счетчик выстрелов
+let guesses = 0;
 
 while (isSunk === false) {
   // не забываем преврать строку в число
@@ -28,6 +30,8 @@ while (isSunk === false) {
   if (guess < 0 || guess > 6) {
     alert("Please, enter a valid cell number!");
   } else {
+    // увеличиваем счетчик выстрелов
+    guesses = guesses + 1;
     // логическое "или"
     if (guess === location1 || guess === location2 || guess === location3) {
       alert("HIT!");
@@ -43,3 +47,6 @@ while (isSunk === false) {
     }
   }
 }
+
+const stats = "You have " + hits + "hits out of " + guesses + "shots.";
+alert(stats);
