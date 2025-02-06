74db1de277cf8bd5e20ef08dabb9c35ab9eb0821
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-06 12:49:37 +0300

    conditions

--- lesson2.js
+++ lesson2.js
@@ -1,11 +1,3 @@
-const local1 = 3;
-const local2 = 4;
-const local3 = 5;
-
-
- if (i = local1 || i = local2 || i = local3) {
-    alert(FIRE!);
-}
-else {
- alert(SLEEP!)
-}
+let userChoice;
+let computerChoice;
+let isWinner = false;
