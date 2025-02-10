66299d59683d7dad2ad00ce4dfc17495d2581d0b
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-10 20:59:06 +0300

    indexOf

--- lesson4.js
+++ lesson4.js
@@ -1,3 +1,24 @@
 scores.push(100);
 console.log(scores); // [82, 75, 91, 85, 93, 88, 99, 100]
 console.log(scores.length); // 8
+
+ 
+// [82, 75, 91, 85, 93, 88, 99, 100]
+ 
+console.log(scores.indexOf(93)) // 4
+console.log(scores.indexOf(101)) // -1
+
+// [82, 75, 91, 85, 93, 88, 99, 100]
+ 
+scores.splice(2, 3) // Удаляем три элемента, начиная с индекса 2
+console.log(scores) // [82, 75, 88, 99, 100]
+
+// [82, 75, 88, 99, 100]
+ 
+scores.splice(3, 0, 77)
+console.log(scores) // [82, 75, 88, 77, 99, 100]
+
+// [82, 75, 88, 77, 99, 100]
+ 
+scores.splice(1, 1, 50)
+console.log(scores) // [82, 50, 88, 77, 99, 100]
