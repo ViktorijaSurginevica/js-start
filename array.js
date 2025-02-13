f6e5640c51a1f5e5a0056de5ca0487f3fe03d56e
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-13 22:43:24 +0300

    array1

---
+++ array.js
@@ -0,0 +1,7 @@
+const animal = ["Кошка", "Мышка", "Собака"];
+const verb = ["нюхала", "курила", "жевала"];
+const noun = ["сыр", "морковка", "макароны"];
+
+let phrase = Math.floor(Math.random()*3;
+
+alert phrase;
