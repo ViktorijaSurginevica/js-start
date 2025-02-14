544154f3af3ac4936cd8bccebaca7c597b222972
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-14 19:43:43 +0300

    garage

--- lesson7.js
+++ lesson7.js
@@ -2,3 +2,21 @@ let gold = 100;
 const buildingName = "Tower";
 const buildingPrice = 30;
 
+if (gold >= buildingPrice) {
+  gold = gold - buildingPrice;
+  console.log(`$(buildingName): work complete`);
+} else {
+  console.log(`$(buildingName): need more gold!`);
+}
+
+buildingName = "Garage";
+buildingPrice = 5;
+
+if (gold >= buildingPrice) {
+  gold = gold - buildingPrice;
+  console.log(`$(buildingName): work complete`);
+} else {
+  console.log(`$(buildingName): need more gold!`);
+}
+
+console.log("Gold rest: $(gold)");
