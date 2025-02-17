65bf9ca53ca800a7b57c63d19a73abcf43960f7b
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-17 12:43:15 +0300

    while array

--- array1.js
+++ array1.js
@@ -1,9 +1,31 @@
-const students = ["Tom", "Mark", "Alfa", "Braun"];
-students[7] = "Mike";
-const result = students.reverse();
-// students.reverse()
-console.log(students);
-console.log(result);
-console.log(students === result);
+// const students = ["Tom", "Mark", "Alfa", "Braun"];
+// students[7] = "Mike";
+// const result = students.reverse();
+// // students.reverse()
+// console.log(students);
+// console.log(result);
+// console.log(students === result);
 
+const scores = [
+  20, 15, 20, 48, 75, 54, 25, 61, 47, 52, 14, 98, 14, 75, 25, 65, 95, 14, 15,
+  28, 24,
+];
 
+let i = 0;
+let output;
+
+while (i < scores.length) {
+  output = "Bubble solution #" + i + " score: " + scores[i];
+  console.log(output);
+
+  i = i + 1;
+}
+
+for () {
+    output = "Bubble solution #" + i + " score: " + scores[i];
+    console.log(output);
+}
+
+console.log("Bubbles tests: " + scores.length);
+
+console.log(scores.length);
