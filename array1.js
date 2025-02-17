65a9c1b25ec846c8d26e0237ec48088aac65198f
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-17 13:06:26 +0300

    bestSolution

--- array1.js
+++ array1.js
@@ -7,12 +7,13 @@
 // console.log(students === result);
 
 const scores = [
-  20, 15, 20, 48, 75, 54, 25, 61, 47, 52, 14, 98, 14, 75, 25, 65, 95, 14, 15,
-  28, 24,
+  20, 98, 15, 20, 48, 75, 54, 25, 61, 47, 52, 14, 98, 14, 75, 25, 65, 95, 14,
+  15, 28, 24, 98,
 ];
 
 let i = 0;
 let output;
+let highScore = 0;
 
 // while (i < scores.length) {
 //   output = "Bubble solution #" + i + " score: " + scores[i];
@@ -23,8 +24,21 @@ let output;
 for (let i = 0; i < scores.length; i++) {
   output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
+  if (highScore < scores[i]) {
+    highScore = scores[i];
+  }
 }
 
 console.log("Bubbles tests: " + scores.length);
-
 console.log(scores.length);
+console.log("Highest bubble score: " + highScore);
+
+const bestSolution = [];
+
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] === highScore) {
+    bestSolution.push(i);
+  }
+}
+
+console.log("Solutions with highest score:" + bestSolution);
