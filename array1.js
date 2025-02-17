4b55c8c63aac4bf11c5a917fe383ce548afea2be
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-17 12:24:12 +0300

    empty index

--- array1.js
+++ array1.js
@@ -1,5 +1,9 @@
 const students = ["Tom", "Mark", "Alfa", "Braun"];
+students[7] = "Mike";
 const result = students.reverse();
+// students.reverse()
 console.log(students);
 console.log(result);
 console.log(students === result);
+
+
