61e7e11e3d8313e4177fb4cac193e4bbdb4ae0b3
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-21 19:32:33 +0300

    splice

--- lesson8.js
+++ lesson8.js
@@ -1,4 +1,7 @@
 let scores = [23, 35, 15, 84, 64, 12, 69, 45];
 
 console.log(scores.length);
-console.log(scores.indexOf(84));
+console.log(scores.indexOf(12));
+
+console.log(scores.splice(4, 3));
+console.log(scores);
