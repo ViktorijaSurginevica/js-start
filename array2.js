a8c0d746895a259fd1ac4fa8ae2f249f5ff2611b
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-02-27 13:24:08 +0300

    5 points

--- array2.js
+++ array2.js
@@ -21,6 +21,21 @@ function classifyScores(scores) {
     for (let i = 0; i < scores.length; i++) {
       let grade // Переменная для хранения оценки
       const score = scores[i]
-      // ...
+   
+      if (score >= 90) {
+        grade = 'A'
+      } else if (score >= 80) {
+        grade = 'B'
+      } else if (score >= 70) {
+        grade = 'C'
+      } else if (score >= 50) {
+        grade = 'D'
+      } else {
+        grade = 'F'
+      }
+   
+      classifiedScores.push(grade)
     }
+   
+      return classifiedScores
   }
