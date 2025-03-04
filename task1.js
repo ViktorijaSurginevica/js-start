211b4667a1c2fc5b6fff33e9579dc836182ea117
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-04 11:01:42 +0300

    finish

--- task1.js
+++ task1.js
@@ -1,25 +1,36 @@
-/*
-Реализовать метод `addResource`, который будет добавлять указанное количество ресурсов к уже существующим ресурсам в объекте `game`.
-
-Требования к методу:
+// Необходимо реализовать метод removeResource в объекте game, который будет уменьшать количество ресурсов в зависимости от переданных параметров.
 
 
-2. Если тип ресурса не существует в объекте `game.resources`, метод должен выводить в консоль сообщение "Invalid resource".
-*/
 
-const game = {
-    resources: {
-      gold: 250,
-      lumber: 100,
-    },
-    addResource(resource, amount) {
-      if (this.resources.hasOwnProperty(resource)) {  
-        // Если ресурс существует, добавляем к нему количество  
-        this.resources[resource] += amount;  
-        console.log(`${amount} ${resource} added. New amount: ${this.resources[resource]}`);  
-      } else {  
-        console.log("Invalid resource");  
-      }  
-    }  
-  }
+// resource (строка) — тип ресурса (gold, lumber и т.д.).
+// amount (число) — количество ресурса, которое нужно убрать.
+
+
+// Если ресурс существует, метод должен проверять, достаточно ли количества ресурса для уменьшения. 
+// Если количество ресурсов меньше, чем amount, выводить сообщение "Not enough resources", 
+// иначе уменьшать количество приростов на переданное значение amount и выводить сообщение с новым количеством.
+
+
+const game = {  
+    resources: {  
+      gold: 250,  
+      lumber: 100,  
+    },  
+    removeResource(resource, amount) {
+        if (this.resourses.hasOwnProperty(resource)) {
+            console.log();
+            
+        } else {
+            console.log("Not enough resources");
+            
+        }
+
+
+    }
+
+  };  
   
+  // Примеры использования метода:  
+  // game.removeResource("gold", 50);   // New amount: 200  
+  // game.removeResource("lumber", 120); // Not enough resources  
+  // game.removeResource("stone", 10);   // Invalid resource  32
