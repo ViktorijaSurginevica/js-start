616b9e5a9b0ff30a335812168a290acde8c4ffac
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-13 20:40:01 +0300

    method update

--- types.js
+++ types.js
@@ -23,4 +23,16 @@ const data = {
     this.course.lessons.push(newLesson);
     this.render();
   },
+
+  updateLessonStatus(lessonId){
+   const lesson = this.course.lessons.find(l => l.id === lessonId)
+   lesson.isDone = !lesson.isDone
+   this.render()
+  }
+
+  updateLessonTitle(lessonId, newTitle){
+    const lesson = this.course.lessons.find(l => l.id === lessonId)
+    lesson.title = newTitle
+    this.render()
+  }
 };
