2e7d1ccd78ecb954c2d8255f9f4194b1e75ad069
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-13 19:41:17 +0300

    id

---
+++ types.js
@@ -0,0 +1,10 @@
+const data = {
+    course: {
+        title: Основы Fron-end разработки,
+        lessons: [
+            {id: 1, title: 'Введение в JavaScript', isDone: true},
+            {id: 2, title: "Операторы, сравнение, ветвление", isDone: false},
+            {id: 3, title: "Функции", isDone: false},
+        ]
+    }
+}
