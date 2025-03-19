d652c91fad7aaea9b13fc0f5ab5de8fd9dc4959e
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-19 14:25:17 +0300

    array for

--- task1.js
+++ task1.js
@@ -1,91 +1,102 @@
-// Задача:
-// Создайте объект car, который будет содержать следующие свойства:
+// // Задача:
+// // Создайте объект car, который будет содержать следующие свойства:
 
-// brand (марка автомобиля)
-// model (модель автомобиля)
-// year (год выпуска)
-// mileage (пробег)
-// Также добавьте метод getCarInfo, который будет возвращать строку с полной информацией о автомобиле в формате:
+// // brand (марка автомобиля)
+// // model (модель автомобиля)
+// // year (год выпуска)
+// // mileage (пробег)
+// // Также добавьте метод getCarInfo, который будет возвращать строку с полной информацией о автомобиле в формате:
 
-// "Марка: [brand], Модель: [model], Год: [year], Пробег: [mileage] км"
+// // "Марка: [brand], Модель: [model], Год: [year], Пробег: [mileage] км"
 
-// let car = {
-//   brand: "Toyota",
-//   model: "Corolla",
-//   year: 2020,
-//   mileage: 15000,
+// // let car = {
+// //   brand: "Toyota",
+// //   model: "Corolla",
+// //   year: 2020,
+// //   mileage: 15000,
 
-//   getCarInfo: function () {
-//     return `Марка: ${this.brand}, Модель: ${this.model}, Год: ${this.year}, Пробег: ${this.mileage} км`;
-//   },
-// };
+// //   getCarInfo: function () {
+// //     return `Марка: ${this.brand}, Модель: ${this.model}, Год: ${this.year}, Пробег: ${this.mileage} км`;
+// //   },
+// // };
 
-// Вывод информации о автомобиле
-console.log(car.getCarInfo());
+// // Вывод информации о автомобиле
+// console.log(car.getCarInfo());
 
-// Создайте объект student, который будет представлять студента и иметь следующие свойства:
+// // Создайте объект student, который будет представлять студента и иметь следующие свойства:
 
-// name (имя студента)
-// age (возраст студента)
-// courses (массив с названиями курсов, которые изучает студент)
-// Добавьте метод addCourse, который будет принимать название курса в качестве аргумента и добавлять его в массив courses. Также добавьте метод getInfo, который будет возвращать строку с информацией о студенте в формате:
+// // name (имя студента)
+// // age (возраст студента)
+// // courses (массив с названиями курсов, которые изучает студент)
+// // Добавьте метод addCourse, который будет принимать название курса в качестве аргумента и добавлять его в массив courses. Также добавьте метод getInfo, который будет возвращать строку с информацией о студенте в формате:
 
-// "Имя: [name], Возраст: [age], Курсы: [courses]"
+// // "Имя: [name], Возраст: [age], Курсы: [courses]"
 
-const student = {  
-    name: "Алексей",  
-    age: 20,  
-    courses: [],  
+// // const student = {  
+//     name: "Алексей",  
+//     age: 20,  
+//     courses: [],  
 
-    addCourse: function(course) {  
-        this.courses.push(course);  
-    },  
+//     addCourse: function(course) {  
+//         this.courses.push(course);  
+//     },  
 
-    getInfo: function() {  
-        return `Имя: ${this.name}, Возраст: ${this.age}, Курсы: ${this.courses.join(", ")}`;  
-    }  
-};  
+//     getInfo: function() {  
+//         return `Имя: ${this.name}, Возраст: ${this.age}, Курсы: ${this.courses.join(", ")}`;  
+//     }  
+// };  
 
-// Добавляем курсы  
-student.addCourse("Математика");  
-student.addCourse("Программирование");  
+// // Добавляем курсы  
+// student.addCourse("Математика");  
+// student.addCourse("Программирование");  
 
-// Вывод информации о студенте  
-console.log(student.getInfo());  
+// // Вывод информации о студенте  
+// console.log(student.getInfo());  
 
 
 
-const library = {  
-    name: "City Library",  
-    books: [],  
+// const library = {  
+//     name: "City Library",  
+//     books: [],  
 
-    addBook(title, author) {  
-        const newBook = {  
-            title: title,  
-            author: author,  
-            isAvailable: true,  
-        };  
-        this.books.push(newBook);  
-    },  
+//     addBook(title, author) {  
+//         const newBook = {  
+//             title: title,  
+//             author: author,  
+//             isAvailable: true,  
+//         };  
+//         this.books.push(newBook);  
+//     },  
 
-    removeBook(title) {  
-        this.books = this.books.filter(book => book.title !== title);  
-    },  
+//     removeBook(title) {  
+//         this.books = this.books.filter(book => book.title !== title);  
+//     },  
 
-    listAvailableBooks() {  
-        return this.books.filter(book => book.isAvailable);  
-    },  
+//     listAvailableBooks() {  
+//         return this.books.filter(book => book.isAvailable);  
+//     },  
 
-    listAllBooks() {  
-        this.books.forEach(book => {  
-            console.log(`${book.title} by ${book.author} - ${book.isAvailable ? "Available" : "Not Available"}`);  
-        });  
-    }  
-};  
+//     listAllBooks() {  
+//         this.books.forEach(book => {  
+//             console.log(`${book.title} by ${book.author} - ${book.isAvailable ? "Available" : "Not Available"}`);  
+//         });  
+//     }  
+// };  
 
-// Пример использования:  
-library.addBook("1984", "George Orwell");  
-library.addBook("To Kill a Mockingbird", "Harper Lee");  
-library.listAllBooks();  
-library.removeBook("1984");  
-library.listAllBooks();  
+// // Пример использования:  
+// library.addBook("1984", "George Orwell");  
+// library.addBook("To Kill a Mockingbird", "Harper Lee");  
+// library.listAllBooks();  
+// library.removeBook("1984");  
+// library.listAllBooks();  
+
+
+function countSheeps(sheep) {
+  
+for (let i = 0; i < array.length; i++) {  
+     
+}  
+
+
+
+  }
