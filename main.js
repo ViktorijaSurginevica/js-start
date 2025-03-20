ac74bed39b4931d0de02327e01f55860320f468a
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-20 19:46:53 +0300

    main new topic

---
+++ main.js
@@ -0,0 +1,82 @@
+//CRUD
+//C
+//R
+//U
+//D
+
+const data = {
+    course: {
+        title: "Основы Fron-end разработки",
+        lessons: [
+            {id: crypto.randomUUID(), title: "Введение в JavaScript" , isDone: true},
+            {id: crypto.randomUUID(), title: "Операторы, сравнение, ветвление", isDone: false},
+            {id: crypto.randomUUID(), title: "Функции", isDone: false },
+        ]
+    },
+    render(){
+        // console.log(this.course);
+        document.body.innerHTML = ""
+        const courseUI = CourseUI(this.course)
+        document.body.append(courseUI)
+    },
+    deleteLesson(lessonId){
+        this.course.lessons = this.course.lessons.filter(l => l.id !== lessonId)
+        this.render()
+    },
+    addLesson(lessonTitle){
+        const newLesson = {id: crypto.randomUUID(), title: lessonTitle , isDone: false}
+        this.course.lessons.push(newLesson)
+        this.render()
+    },
+    updateLessonStatus(lessonId){
+        const lesson = this.course.lessons.find(l => l.id === lessonId)
+        lesson.isDone = !lesson.isDone
+        this.render()
+    },
+    updateLessonTitle(lessonId, newTitle){
+        const lesson = this.course.lessons.find(l => l.id === lessonId)
+        lesson.title = newTitle
+        this.render()
+    }
+
+}
+
+data.render()
+
+
+// {
+//     title: "Основы Fron-end разработки",
+//     lessosns: [
+//         {id: crypto.randomUUID(), title: "Введение в JavaScript" , isDone: true},
+//         {id: crypto.randomUUID(), title: "Операторы, сравнение, ветвление", isDone: false},
+//         {id: crypto.randomUUID(), title: "Функции", isDone: false },
+//     ]
+// }
+
+
+function CourseUI (course) {
+    const courseElement = document.createElement("div")
+    courseElement.classList.add("course")
+
+    const courseTitleElement = document.createElement("h1")
+    courseTitleElement.classList.add("title")
+    courseTitleElement.textContent = course.title
+
+    const courseListElement = document.createElement("ol")
+    courseListElement.classList.add("list")
+
+    const lessons = course.lessons
+
+    for (let i = 0; i < lessons.length; i++) {
+        const lessonElement = document.createElement("li")
+        lessonElement.classList.add("item")
+        lessonElement.textContent = lessons[i].title
+        courseListElement.append(lessonElement)
+    }
+
+    courseElement.append(courseTitleElement)
+    courseElement.append(courseListElement)
+
+
+    return courseElement
+}
---
+++ main2.js
@@ -0,0 +1,94 @@
+//CRUD
+//C
+//R
+//U
+//D
+// model
+const data = {
+  course: {
+    title: "Основы Fron-end разработки",
+    lessons: [
+      { id: crypto.randomUUID(), title: "Введение в JavaScript", isDone: true },
+      {
+        id: crypto.randomUUID(),
+        title: "Операторы, сравнение, ветвление",
+        isDone: false,
+      },
+      { id: crypto.randomUUID(), title: "Функции", isDone: false },
+    ],
+  },
+  render() {
+    // console.log(this.course);
+    document.body.innerHTML = "";
+    const courseUI = CourseUI(this.course);
+    document.body.append(courseUI);
+  },
+  deleteLesson(lessonId) {
+    this.course.lessons = this.course.lessons.filter((l) => l.id !== lessonId);
+    this.render();
+  },
+  addLesson(lessonTitle) {
+    const newLesson = {
+      id: crypto.randomUUID(),
+      title: lessonTitle,
+      isDone: false,
+    };
+    this.course.lessons.push(newLesson);
+    this.render();
+  },
+  updateLessonStatus(lessonId) {
+    const lesson = this.course.lessons.find((l) => l.id === lessonId);
+    lesson.isDone = !lesson.isDone;
+    this.render();
+  },
+  updateLessonTitle(lessonId, newTitle) {
+    const lesson = this.course.lessons.find((l) => l.id === lessonId);
+    lesson.title = newTitle;
+    this.render();
+  },
+};
+
+// view
+data.render();
+
+// {
+//     title: "Основы Fron-end разработки",
+//     lessosns: [
+//         {id: crypto.randomUUID(), title: "Введение в JavaScript" , isDone: true},
+//         {id: crypto.randomUUID(), title: "Операторы, сравнение, ветвление", isDone: false},
+//         {id: crypto.randomUUID(), title: "Функции", isDone: false },
+//     ]
+// }
+
+function CourseUI(course) {
+  const courseElement = document.createElement("div");
+  courseElement.classList.add("course");
+
+  courseElement.append(CourseTitle(course.title), LessonsList(course.lessons));
+  return courseElement;
+}
+
+function CourseTitle(title) {
+  const rootElement = document.createElement("h1");
+  rootElement.classList.add("title");
+  rootElement.textContent = title;
+  return rootElement;
+}
+
+function LessonsList(lessons) {
+  const rootElement = document.createElement("ol");
+  rootElement.classList.add("list");
+  for (let i = 0; i < lessons.length; i++) {
+    const lessonElement = document.createElement("li");
+    lessonElement.classList.add("item");
+    lessonElement.textContent = lessons[i].title;
+    rootElement.append(lessonElement);
+  }
+  return rootElement;
+}
+
+function Lesson(lesson) {
+  const rootElement = document.createElement("li");
+  rootElement.classList.add("item");
+  rootElement.textContent = lesson.title;
+}
